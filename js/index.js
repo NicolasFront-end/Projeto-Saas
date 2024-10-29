f46886156a2f272b91d6095837a8c81cf5edf233
Nicolas Mota <nicolasmotta97@outlook.com>
2024-10-29 10:12:44 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,11 @@
+//Passo 1 - Criar uma variável no JS pra trabalhar com os cards
+const listaSelecaoUserCard = document.querySelectorAll(".user-card");
+console.log(listaSelecaoUserCard);
+
+//PASSO 2 - Identificar o evento de clique no elemento da listagem
+listaSelecaoUserCard.forEach(usercard =>{
+    usercard.addEventListener("click", () =>{
+        
+    })
+    
+})
